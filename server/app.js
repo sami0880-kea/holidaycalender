0388396602ad1d6a5b9a07949815e3b23b551fd6
Samim S. <samimsaidi2660@gmail.com>
2024-04-27 15:48:38 +0200

    Build folder in server, serving static

--- server/app.js
+++ server/app.js
@@ -9,13 +9,13 @@ const PORT = process.env.PORT ?? 8080;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.resolve('../client/build')));
+app.use(express.static(path.resolve('./build')));
 
 // Routers
 app.use(holidayRouter);
 
 app.get("*", (req, res) => {
-    res.sendFile(path.resolve('../client/build/index.html'))
+    res.sendFile(path.resolve('./build/index.html'))
 });
 
 app.listen(PORT, () => console.log("Server running on port", PORT));
